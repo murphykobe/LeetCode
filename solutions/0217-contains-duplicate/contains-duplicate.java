dfb314c2cb2887db5e8bfb1a277430e2e5c350d7
Murphy Liu <murphykobe@gmail.com>
2019-06-26 22:24:46 -0400

    Update -0217 sorting method

--- solutions/0217-contains-duplicate/contains-duplicate.java
+++ solutions/0217-contains-duplicate/contains-duplicate.java
@@ -24,15 +24,13 @@
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
+        if(nums==null || nums.length==0) return false;
         Arrays.sort(nums);
-        boolean flag=true;
-        for(int i=0;i<nums.length-1;i++){
-            if(nums[i]==nums[i+1]){
-                flag=false;
-                break;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                return true;
             }
         }
-        return !flag;
-        
+        return false;
     }
 }
