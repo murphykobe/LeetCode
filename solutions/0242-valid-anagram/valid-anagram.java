095da67ab23512aa9b1c90a7c10f0f71beca2ade
Murphy Liu <murphykobe@gmail.com>
2019-06-26 23:21:06 -0400

    Update O(N) method on valid anagram

--- solutions/0242-valid-anagram/valid-anagram.java
+++ solutions/0242-valid-anagram/valid-anagram.java
@@ -26,11 +26,16 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
         return false;
-    }
-    char[] str1 = s.toCharArray();
-    char[] str2 = t.toCharArray();
-    Arrays.sort(str1);
-    Arrays.sort(str2);
-    return Arrays.equals(str1, str2);
+        }
+        
+        int[] map=new int[26];
+        for(int i=0;i<s.length();i++){
+            map[s.charAt(i)-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            map[t.charAt(i)-'a']--;
+            if(map[t.charAt(i) - 'a'] < 0) return false;
+        }
+        return true;
     }
 }
